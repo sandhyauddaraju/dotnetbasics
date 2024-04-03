b0a522f168f1a6efd18aa48b128b6dcc5b88ef62
sandhyauddaraju <sandhyauddaraju8@gmail.com>
2024-04-03 19:21:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace MyApp
+{
+    internal class Employee1 : Person
+    {
+        protected override void Work()
+        {
+            base.Work();
+        }
+        static void Main(string[] args)
+        {
+            Employee1 p = new Employee1();
+            p.Address = "NewYork"; //protected internal
+            p.Name = "Test"; //protected
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
